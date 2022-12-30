5e4c80d796bc0ae03debaca426e0a64a74eeda5e
vishalicious213 <vishalicious213@gmail.com>
2022-12-30 07:40:49 -0800

    made app more performant by saving elements to be rendered into a variable and then rendering them once, instead of doing it once per element

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const savedInputsEl = document.getElementById("saved-inputs-el")
 let savedInputs = ["www.url1.com", "www.url2.com", "www.url3.com"]
+let savedInputsList = ""
 
 inputBtn.addEventListener("click", function() {
     savedInputs.push(inputEl.value)
@@ -9,12 +10,13 @@ inputBtn.addEventListener("click", function() {
 
 function showSavedInputs() {
     for (let i = 0; i < savedInputs.length; i++) {
-        savedInputsEl.innerHTML += `<li>${savedInputs[i]}</li>`
+        savedInputsList += `<li>${savedInputs[i]}</li>`
         // THIS IS HOW TO DO THE ABOVE USING JAVASCRIPT:
         // const li = document.createElement("li") // create element
         // li.textContent = savedInputs[i] // set text content
         // savedInputsEl.append(li) // append to ul
     }
+    savedInputsEl.innerHTML = savedInputsList
 }
 
 showSavedInputs()
