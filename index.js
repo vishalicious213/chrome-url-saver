01c8de9aee617d8e3b9b923e54adaf8d20196274
vishalicious213 <vishalicious213@gmail.com>
2022-12-30 07:46:59 -0800

    moved savedInputsList string into showSavedInputs() and made input button event listener called showSavedInputs when clicked. removed dummy data from savedInputs, too. clicking button will now append input to savedInputs array and render that array onscreen

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const savedInputsEl = document.getElementById("saved-inputs-el")
-let savedInputs = ["www.url1.com", "www.url2.com", "www.url3.com"]
-let savedInputsList = ""
+let savedInputs = []
 
 inputBtn.addEventListener("click", function() {
     savedInputs.push(inputEl.value)
+    showSavedInputs()
 })
 
 function showSavedInputs() {
+    let savedInputsList = ""
     for (let i = 0; i < savedInputs.length; i++) {
         savedInputsList += `<li>${savedInputs[i]}</li>`
         // THIS IS HOW TO DO THE ABOVE USING JAVASCRIPT:
