892f6b6269583d449d19a9cd9f11c5864f43f3b1
vishalicious213 <vishalicious213@gmail.com>
2022-12-30 07:20:16 -0800

    rendering savedInputs onscreen using innerHTML. moved that functionality into its own function and set it to run automatically instead of when button is clicked

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const savedInputsEl = document.getElementById("saved-inputs-el")
 let savedInputs = ["www.url1.com", "www.url2.com", "www.url3.com"]
 
 inputBtn.addEventListener("click", function() {
     savedInputs.push(inputEl.value)
-    // console.log(savedInputs)
-    showSavedInputs()
 })
 
 function showSavedInputs() {
     for (let i = 0; i < savedInputs.length; i++) {
-        console.log(savedInputs[i])
+        savedInputsEl.innerHTML += `<li>${savedInputs[i]}</li>`
     }
 }
+
+showSavedInputs()
