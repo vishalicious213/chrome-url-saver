86403b8bdf1262d71d529c6d9ed8520880ce2b7c
vishalicious213 <vishalicious213@gmail.com>
2022-12-30 19:56:41 -0800

    added event listener to delete all button. it deletes URLs from localStorage, savedInputs array and renders empty array

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const deleteBtn = document.getElementById("delete-btn")
 const savedInputsEl = document.getElementById("saved-inputs-el")
 let savedInputs = []
-let urlsFromLocalStorage = JSON.parse(localStorage.getItem("saved-urls"))
+const urlsFromLocalStorage = JSON.parse(localStorage.getItem("saved-urls"))
 
 // function to add URL to array and save to localStorage
 inputBtn.addEventListener("click", function() {
@@ -10,6 +11,13 @@ inputBtn.addEventListener("click", function() {
     showSavedInputs()
     inputEl.value = "" // clear input field after text is saved
     localStorage.setItem("saved-urls", JSON.stringify(savedInputs)) // save URL to localStorage
+})
+
+// function to remove all URLs from localStorage, array and onscreen
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear() // remove URLs from localStorage
+    savedInputs = [] // set savedInputs to empty array
+    showSavedInputs() // render empty array
 })
 
 // function to display saved URLs
